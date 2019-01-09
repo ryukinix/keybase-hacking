401512f31ca20af93fb45f3de6d198090ba8b5e6
Manoel Vilela <manoel_vilela@engineer.com>
2019-01-09 01:29:34 -0300

    Fix crack.py: exit gracefully and save result to filesystem

--- crack.py
+++ crack.py
@@ -11,8 +11,9 @@
 
 from tqdm import tqdm
 import os
+import sys
 
-banner="""\
+banner = """\
 ██ ▄█▀▓█████▓██   ██▓ ▄▄▄▄    ▄▄▄        ██████ ▓█████
  ██▄█▒ ▓█   ▀ ▒██  ██▒▓█████▄ ▒████▄    ▒██    ▒ ▓█   ▀
 ▓███▄░ ▒███    ▒██ ██░▒██▒ ▄██▒██  ▀█▄  ░ ▓██▄   ▒███
@@ -43,28 +44,35 @@ def crack(commands):
         status = os.system(command)
         if status == 0:
             answer = command
+            break
 
     return answer
 
 
 def main():
     print(banner)
+    print("CRACKING KEYBASE BY BRUTE FORCE!!!")
+    a = input('Are you ready? [Y/n]')
+    if a.lower().startswith('n'):
+        return
     # load list of commands to try
     # 0 status means success
     # otherwise error
     with open("session/commands.txt") as f:
         commands = list(map(str.strip, f.readlines()))
 
-    print("CRACKING KEYBASE BY BRUTE FORCE!!!")
-    answer = crack(commands)
+    answer = crack(commands[13300:])
     if answer:
         print("CRACKED! :]")
         print("This command had success:")
         print(answer)
-        with open("session/result.txt", 'w') as f:
+        with open("session/paperkey.txt", 'w') as f:
             f.write(answer)
+        print("Result saved in session/paperkey.txt")
     else:
         print("None guess have success. Cracking didn't work! :[")
+
+    os._exit(0)
 
 
 if __name__ == '__main__':
